f690b11b7ed5d3dee3b82bd8b4533f1a0593de56
GTtron816 <abinak322@gmail.com>
2024-03-06 03:46:57 +0000

    add firebase connection

--- detect.py
+++ detect.py
@@ -6,16 +6,24 @@ from mediapipe.tasks.python import vision
 from utils import visualize
 import threading
 import time
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate('/home/gttron/AgroProtect_Detection/agroprotect-e0049-firebase-adminsdk-ynhkq-10957fb18a.json')  # replace with your actual path
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://agroprotect-e0049-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
 app = Flask(__name__)
 
 # Global variables
 frame = None
+ref = db.reference('/detected_classes')
 
 def generate():
     global frame
     while True:
         if frame is not None:
-            # Encode the frame in JPEG format
             _, buffer = cv2.imencode('.jpg', frame)
             frame_bytes = buffer.tobytes()
             yield (b'--frame\r\n'
@@ -25,20 +33,25 @@ def generate():
 def index():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def update_firebase(labels):
+    if labels:
+        ref.child('label').set(', '.join(labels))
+        ref.child('switch').set(True)
+    else:
+        ref.child('label').set("")
+        ref.child('switch').set(False)
+
 def run(model: str, max_results: int, score_threshold: float, camera_id: int, width: int, height: int) -> None:
-    # Start capturing video input from the camera
     cap = cv2.VideoCapture(camera_id)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    # Global variables to calculate FPS
     COUNTER, FPS = 0, 0
     START_TIME = time.time()
 
-    # Visualization parameters
-    row_size = 50  # pixels
-    left_margin = 24  # pixels
-    text_color = (0, 0, 0)  # black
+    row_size = 50
+    left_margin = 24
+    text_color = (0, 0, 0)
     font_size = 1
     font_thickness = 1
     fps_avg_frame_count = 10
@@ -49,15 +62,19 @@ def run(model: str, max_results: int, score_threshold: float, camera_id: int, wi
     def save_result(result: vision.ObjectDetectorResult, unused_output_image: mp.Image, timestamp_ms: int):
         nonlocal FPS, COUNTER, START_TIME, detection_frame
 
-        # Calculate the FPS
         if COUNTER % fps_avg_frame_count == 0:
             FPS = fps_avg_frame_count / (time.time() - START_TIME)
             START_TIME = time.time()
-
         detection_result_list.append(result)
         COUNTER += 1
+        if result.detections:
+            labels = [category.category_name for category in result.detections[0].categories
+                      if category.category_name != 'human' and category.score > 0.7]
+            if labels:
+                threading.Thread(target=update_firebase, args=(labels,)).start()
+            else:
+                threading.Thread(target=update_firebase, args=(None,)).start()
 
-    # Initialize the object detection model
     base_options = python.BaseOptions(model_asset_path=model)
     options = vision.ObjectDetectorOptions(base_options=base_options,
                                            running_mode=vision.RunningMode.LIVE_STREAM,
@@ -65,7 +82,6 @@ def run(model: str, max_results: int, score_threshold: float, camera_id: int, wi
                                            result_callback=save_result)
     detector = vision.ObjectDetector.create_from_options(options)
 
-    # Continuously capture images from the camera and run inference
     while cap.isOpened():
         success, image = cap.read()
         image = cv2.resize(image, (640, 480))
@@ -73,15 +89,11 @@ def run(model: str, max_results: int, score_threshold: float, camera_id: int, wi
             break
 
         image = cv2.flip(image, 1)
-
-        # Convert the image from BGR to RGB as required by the TFLite model.
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
 
-        # Run object detection using the model.
         detector.detect_async(mp_image, time.time_ns() // 1_000_000)
 
-        # Show the FPS
         fps_text = 'FPS = {:.1f}'.format(FPS)
         text_location = (left_margin, row_size)
         current_frame = image
@@ -97,7 +109,6 @@ def run(model: str, max_results: int, score_threshold: float, camera_id: int, wi
             global frame
             frame = detection_frame
 
-        # Stop the program if the ESC key is pressed.
         if cv2.waitKey(1) == 27:
             break
 
@@ -106,7 +117,7 @@ def run(model: str, max_results: int, score_threshold: float, camera_id: int, wi
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    # Start Flask server in a separate thread
+    # Start Flask server
     flask_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
     flask_thread.daemon = True
     flask_thread.start()
